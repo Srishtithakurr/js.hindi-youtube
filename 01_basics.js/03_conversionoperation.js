64dddeaadb7907d407ef00b7a6c00a5ded82090b
Srishtithakurr <25bee107@nith.ac.in>
2026-06-02 15:37:24 +0000

    conversion operations compeleted wigth reality table

---
+++ 01_basics.js/03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = 33
+console.log(score) //prints 33
+
+let wrong_score = "33abc"
+console.log(wrong_score)//prints 33abc
+
+//now trying to convert it into number
+console.log(Number(wrong_score))//prints NaN(means not a number)
+
+let srishti = true
+let according_to_me = Number(srishti)
+let srish = false
+let according_to_Parth = Number(srish)
+console.log(according_to_me,according_to_Parth)
+
+//now we will convert into boolean
+let Srishti = 0
+let Parth = 1
+console.log(Boolean(Srishti,Parth))//will print only false 
+//Boolean designed for only one single value
+
+console.table([Boolean(Srishti),Boolean(Parth)]) //correct way
+
+const the_reality_table = [
+    { name: 'Srishti', result: Boolean(Srishti)},
+    { name: 'Parth', result: Boolean(Parth)}
+]
+
+console.table(the_reality_table)
+console.log(the_reality_table)

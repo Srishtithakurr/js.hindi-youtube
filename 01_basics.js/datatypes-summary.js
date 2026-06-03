982df7be2bc6ba4708041b99ba32e4cad66759bb
Srishtithakurr <25bee107@nith.ac.in>
2026-06-03 23:58:43 +0000

    done with heap and stack memory

--- 01_basics.js/datatypes-summary.js
+++ 01_basics.js/datatypes-summary.js
@@ -17,3 +17,28 @@
 //Functions
 
 //Javascript is dynamically typed
+
+
+//stack(primitive)
+//heap(non-primitive)
+
+//firstly we will see how stack memory works
+let nickname = "Srishti"
+let official_name = nickname
+
+official_name = "Srishti Thakur"
+console.log(official_name); // Prints: "Srishti" (Unchanged!)
+console.log(nickname); // Prints: "Srishti Thakur"
+
+//secondly I will write an example for heap memory
+let userOne = {
+    email : "Parthbansal2007@gmail.com" ,
+    upi_id : "rekhabansal@ybl"
+}
+ let userTwo = userOne //copies refrence address not the actual address
+  
+
+  userTwo.email = "srishtithakur2024@gmail.com"//modified shared data in the heap
+
+console.log(userOne.email)
+console.log(userTwo.email)
